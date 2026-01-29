e6df11906a4f42b37596e9baae0772e49a4ac9ec
S KARTHIK <skarthik.link@gmail.com>
2026-01-29 15:21:49 +0530

    [S Karthik] Added: UC8 final OOPS design

--- src/FactoryRobotHazardAnalyzer.java
+++ src/FactoryRobotHazardAnalyzer.java
@@ -2,25 +2,9 @@ import java.util.Scanner;
 
 public class FactoryRobotHazardAnalyzer {
 
-    public static int calculateRisk(int hazardLevel) throws RobotSafetyException {
-        if (hazardLevel < 1 || hazardLevel > 10) {
-            throw new RobotSafetyException("Hazard level must be between 1 and 10");
-        }
-        return hazardLevel * 10;
-    }
-
-    public static String classifyRisk(int hazardLevel) {
-        if (hazardLevel >= 1 && hazardLevel <= 3) {
-            return "LOW";
-        } else if (hazardLevel <= 6) {
-            return "MEDIUM";
-        } else {
-            return "HIGH";
-        }
-    }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        RobotHazardService service = new RobotHazardService();
 
         try {
             System.out.print("Enter robot name: ");
@@ -29,10 +13,10 @@ public class FactoryRobotHazardAnalyzer {
             System.out.print("Enter hazard level (1-10): ");
             int hazardLevel = scanner.nextInt();
 
-            int riskScore = calculateRisk(hazardLevel);
-            String riskCategory = classifyRisk(hazardLevel);
+            int riskScore = service.calculateRisk(hazardLevel);
+            String riskCategory = service.classifyRisk(hazardLevel);
 
-            System.out.println("Robot: " + robotName);
+            System.out.println("Robot Name: " + robotName);
             System.out.println("Risk Score: " + riskScore);
             System.out.println("Risk Category: " + riskCategory);
 
