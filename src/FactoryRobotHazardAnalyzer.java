0200ce5888b11224edc40f29ab44a3ca1ff60670
S KARTHIK <skarthik.link@gmail.com>
2026-01-29 14:51:33 +0530

    [S Karthik] Added: UC2 accept robot input

--- src/FactoryRobotHazardAnalyzer.java
+++ src/FactoryRobotHazardAnalyzer.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class FactoryRobotHazardAnalyzer {
     public static void main(String[] args) {
-        System.out.println("Factory Robot Hazard Analyzer");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter robot name: ");
+        String robotName = scanner.nextLine();
+
+        System.out.println("Analyzing hazards for robot: " + robotName);
+        scanner.close();
     }
 }
